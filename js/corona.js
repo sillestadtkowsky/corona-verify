ee0d26a57e6ffec8d43c1d3733f8f0f653485ae3
sillestadtkowsky <silvio.osowsky@gmail.com>
2021-11-24 16:50:43 +0100

    add QR Code

---
+++ js/corona.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function () {
+    console.log("loaded")
+});
+
+function defer(method) {
+    if (window.jQuery) {
+        method();
+    } else {
+        setTimeout(function() { defer(method) }, 50);
+    }
+}
